fc509cd113a255f2461d0ebb3af7e83fd91a48f9
SuMoThet <sumonthet.92@gmail.com>
2015-08-16 01:08:40 +0800

    Good

--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
-            
+            int n, remainder, sum = 0;
+            Console.Write("Enter a number : ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = n; i > 0; i = i / 10)
+            {
+                remainder = i % 10;
+                sum = sum + remainder * remainder * remainder;
+            }
+            if (sum == n)
+            {
+                Console.Write("Entered number is an Armstrong number.");
+            }
+            else
+                Console.Write("Entered number is not an Armstrong number.");
+            Console.ReadLine();
         }
     }
 }
